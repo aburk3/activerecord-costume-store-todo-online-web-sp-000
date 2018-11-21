e988f6cf63d0c6673627bc5c3c034519770d2a19
Austin  Burke <aburk3@gmail.com>
2018-11-21 11:06:29 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,6 +6,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.0]
       t.string :theme
       t.float :price
       t.boolean :family
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :long_description
     end
   end
 end
