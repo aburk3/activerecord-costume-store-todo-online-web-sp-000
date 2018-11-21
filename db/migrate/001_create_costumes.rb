190639c48517c4bef8819697d81148596e8a680e
Austin  Burke <aburk3@gmail.com>
2018-11-21 10:45:18 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,1 +1,16 @@
 # Create a class and inherit from ActiveRecord::Migration
+<<<<<<< HEAD
+=======
+class CreateCostumes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :costumes do |t|
+      t.float :price
+      t.string :name
+      t.string :size
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
+>>>>>>> 8040cd11fae8e51e0c1d7f503ec7dcf8eec91c9c
