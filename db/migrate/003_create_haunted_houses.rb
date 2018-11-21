022e7891bd8da8ad9bcff806c9b64600d707176a
Austin  Burke <aburk3@gmail.com>
2018-11-21 11:05:36 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,6 +2,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.0]
   def change
     create_table :haunted_houses do |t|
       t.string :name
+      t.string :location
+      t.string :theme
     end
   end
 end
