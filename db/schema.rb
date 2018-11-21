f2a58679e14205d20e4a8d644103c96d2cd81e7f
Austin  Burke <aburk3@gmail.com>
2018-11-21 11:07:44 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.string "theme"
     t.float "price"
-    t.boolean "family"
+    t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
     t.string "long_description"
