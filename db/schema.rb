aa978c0269e2ac0bdaff24bd025e7b40fd60d63e
Austin  Burke <aburk3@gmail.com>
2018-11-21 11:07:10 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.string "long_description"
   end
 
 end
